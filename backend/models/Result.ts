05d7f3d483bcd99fe21a3bc8fda42c426b0b51fa
Flo Slv <florent3969@hotmail.fr>
2023-03-11 12:16:16 +0100

    🟦 feat: add model Result

---
+++ backend/models/Result.ts
@@ -0,0 +1,9 @@
+import { model, Schema } from "mongoose";
+
+const resultSchema = new Schema({
+  nickName: String,
+  score: Number,
+  date: String,
+});
+
+export default model("Result", resultSchema);
