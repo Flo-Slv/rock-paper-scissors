94748fb5860be9724551db98284b282c6d3b9df2
Flo Slv <florent3969@hotmail.fr>
2023-03-13 14:14:33 +0100

    🟦 feat: ApolloClient setup with React

--- rockpaperscissors/client/index.tsx
+++ rockpaperscissors/client/index.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 import App from "./App";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+  cache: new InMemoryCache(),
+});
 
 const app = ReactDOM.createRoot(document.getElementById("app") as HTMLElement);
 
 app.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
