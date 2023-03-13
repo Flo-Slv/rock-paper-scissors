a779cc69ef2f8491a9bb41ff9c8bed1e25abf704
Flo Slv <florent3969@hotmail.fr>
2023-03-13 17:33:07 +0100

    🟦 feat: WIP - Players component

---
+++ rockpaperscissors/client/components/Players.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import clsx from "clsx";
+
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+
+import useStore from "../utils/zustand/store";
+
+const Players = () => {
+  const nickName = useStore((state) => state.nickName);
+  const userChoice = useStore((state) => state.userChoice);
+  const userScore = useStore((state) => state.userScore);
+  const computerScore = useStore((state) => state.computerScore);
+  const computerChoice = useStore((state) => state.computerChoice);
+
+  return (
+    <div
+      className={clsx("text-xl", "flex flex-col md:flex md:flex-row md:gap-16")}
+    >
+      <div className="w-[250px] rounded-xl border border-gray-800 bg-gray-900 p-4 shadow-xl sm:p-6 lg:p-8">
+        <h3 className="mt-3 text-lg font-bold text-white sm:text-xl">
+          {nickName ? nickName : "Enter a nickname"}
+        </h3>
+
+        <p className="mt-4 text-sm text-gray-300">Score: {userScore}</p>
+
+        <p className="mt-4 text-sm text-gray-300">
+          {userChoice ? userChoice : "Please select an item to play"}
+        </p>
+      </div>
+
+      <div>WINNER IS...</div>
+
+      <div className="w-[250px] rounded-xl border border-gray-800 bg-gray-900 p-4 shadow-xl sm:p-6 lg:p-8">
+        <h3 className="mt-3 text-lg font-bold text-white sm:text-xl">
+          Computer
+        </h3>
+
+        <p className="mt-4 text-sm text-gray-300">Score: {computerScore}</p>
+
+        <p className="mt-4 text-sm text-gray-300">
+          {computerChoice ? computerChoice : "Waiting..."}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Players;
